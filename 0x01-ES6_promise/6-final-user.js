a548837c5078230d9b4b85f2d5014f79b7756c19
huy75 <huy.nguyen@holbertonschool.com>
2021-07-15 10:04:22 +0000

    fix new solution

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,23 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const result = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    result.push({ status: 'fulfilled', value: user });
-    await uploadPhoto(fileName);
-  } catch (error) {
-    result.push({
-      status: 'rejected',
-      value: error.toString(),
+export default function handleProfileSignup(firstName, lastName, fileName,) {
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
     });
-  }
-  return result;
 }
