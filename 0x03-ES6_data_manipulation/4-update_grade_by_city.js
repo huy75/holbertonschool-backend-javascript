3aff42fd8efc50790f3a1c7321ec3ef60fd6b017
huy75 <huy.nguyen@holbertonschool.com>
2021-07-20 07:31:06 +0000

    fix eslint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,11 +6,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     return [];
   }
   return students
-  .filter((student) => student.location === city)
-  .map((student) => {
-    const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
-    return {
-      ...student,
-      grade: newGrade ? newGrade.grade : 'N/A' };
-  });
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
 }
