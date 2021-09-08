ef21fd4d0ea87fcdfd8f019fac7c04a9e9d999e9
huy75 <2487@holbertonschool.com>
2021-09-08 09:36:40 +0000

    fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,26 +1,20 @@
 const http = require('http');
 const students = require('./3-read_file_async');
-
+const pathCSV = process.argv.slice(2)[0];
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/':
-      res.writeHead(200);
-      res.end('Hello Holberton School!');
-      break;
-    case '/students':
-      res.writeHead(200);
+const app = http.createServer(async (req, res) => {
+    const { url } = req;
+    if (url === '/') {
+      res.write('Hello Holberton School!');
+      res.end();
+    }
+    if (url === '/students') {
+      const arrStudents = await students(pathCSV);
       res.write('This is the list of our students\n');
-      students(process.argv[2])
-        .then((data) => res.end(data))
-        .catch((error) => res.end(error.message));
-      break;
-    default:
-      res.writeHead(404);
-      res.end(JSON.stringify({ error: 'Resource not found' }));
-  }
-});
+      res.end(`${arrStudents}`);
+    }
+  });
 
 app.listen(port);
 
