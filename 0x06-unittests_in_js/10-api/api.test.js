8fd22e543a6daa929a026e42969e305ecd7b8dc9
huy75 <2487@holbertonschool.com>
2021-09-12 12:30:37 +0000

    feat task 10

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -132,4 +132,78 @@ describe('Integration Testing', () => {
       });
     });
   });
+
+  describe('GET /available_payments JSON string', () => {
+    it('Responds with 200 and correct JSON string', (done) => {
+      const options = {
+        url: 'http://localhost:7865/available_payments',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal(
+          '{"payment_methods":{"credit_cards":true,"paypal":false}}'
+        );
+        done();
+      });
+    });
+  });
+
+  describe('GET /available_payments JSON parsed', () => {
+    it('Responds with 200 and correct JSON object when parsed', (done) => {
+      const options = {
+        url: 'http://localhost:7865/available_payments',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        const bodyParsed = JSON.parse(body);
+
+        const referenceBody = {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false,
+          },
+        };
+
+        expect(bodyParsed).to.deep.equal(referenceBody);
+        done();
+      });
+    });
+  });
+
+  describe('POST /login with body', () => {
+    it('Responds with 200 and correct name Betty', (done) => {
+      const options = {
+        url: 'http://localhost:7865/login',
+        method: 'POST',
+        json: {
+          userName: 'Betty',
+        },
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome Betty');
+        done();
+      });
+    });
+  });
+
+  describe('POST /login with no body', () => {
+    it('Responds with 200 and correct name Undefined', (done) => {
+      const options = {
+        url: 'http://localhost:7865/login',
+        method: 'POST',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome undefined');
+        done();
+      });
+    });
+  });
 });
