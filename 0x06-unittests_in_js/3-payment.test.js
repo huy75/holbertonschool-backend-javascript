ec9524f282b02147a6ce3e3c801cfe66d2fe80e1
huy75 <2487@holbertonschool.com>
2021-09-28 14:36:15 +0000

    add comments

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,19 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
 
-describe('sendPaymentRequestToApi', () => {
-  it('validates usage of Utils.calculateNumber', () => {
-    const calcNumSpy = sinon.spy(Utils, 'calculateNumber');
+describe('sendPaymentRequestToApi function', () => {
+  const utilSpy = sinon.spy(Utils, 'calculateNumber');
+
+  it('validate the usage of the Utils function', () => {
+    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
-
-    expect(calcNumSpy.calledWith('SUM', 100, 20)).to.be.true;
-    calcNumSpy.restore();
+    // check that the spy was called exactly once
+    expect(utilSpy.calledOnce).to.be.true;
+    // check that spy was called at least once with the provided arguments
+    expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
+    utilSpy.restore()
   });
 });
